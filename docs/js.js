a34f62c9b8932568b71970bf929733ab1cef6925
Alex <alkonoplev@yandex.ru>
2021-12-12 05:54:34 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -3,6 +3,6 @@ window.onload = function() {
 console.log('567');  
   var elements = document.getElementsByClassName('jet-headline__label');
   console.log(elements);
-  elements[48].style.color = 'red'
+  elements[46].innerHTML="Овсянников Пётр";
 
 }
