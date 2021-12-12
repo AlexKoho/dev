1fac95bf424512d4fb668c733f4f9c335297a70e
Alex <alkonoplev@yandex.ru>
2021-12-12 05:59:13 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -1,6 +1,5 @@
-console.log('Овсянников Алёша');
 window.onload = function() {
 console.log('567');  
   var elements = document.getElementsByClassName('jet-headline__label');
-  setTimeout(() => {  elements[46].innerHTML="Овсянников Пётр"; }, 5000);  
+  setTimeout(() => {  elements[46].innerHTML="Овсянников Алексей"; }, 5000);  
 }
