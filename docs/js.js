8f70baddb11262f30aba346498d9bd0cee39ecc9
Alex <alkonoplev@yandex.ru>
2021-12-12 05:47:28 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -1,8 +1,7 @@
 console.log('Овсянников Алёша');
 window.onload = function() {
 console.log('567');  
-function changeColor () {
   var elements = document.getElementsByClassName('jet-headline__label');
   console.log(elements);
-}
+
 }
