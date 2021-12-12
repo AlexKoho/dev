61047a9e6e409e91112c56741c2147f68ac03aba
Alex <alkonoplev@yandex.ru>
2021-12-12 09:04:37 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-  //console.log('555');
   //var elements = document.getElementsByClassName('jet-headline__label');
   //setTimeout(() => {  elements[46].innerHTML=""; }, 5000);
   
