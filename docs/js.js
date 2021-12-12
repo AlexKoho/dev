206b9be73181d7aedc389c6607ba17b0a49af014
Alex <alkonoplev@yandex.ru>
2021-12-12 08:53:38 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  console.log('555');
   //var elements = document.getElementsByClassName('jet-headline__label');
   setTimeout(() => {  elements[46].innerHTML="Вася Петров"; }, 5000);  
 }
