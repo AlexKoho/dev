34c46ebe951ef48531c3196868739805c703dc3b
Alex <alkonoplev@yandex.ru>
2021-12-18 00:21:52 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  //var elements = document.getElementsByClassName('jet-headline__label');
-  //setTimeout(() => {  elements[46].innerHTML=""; }, 5000);
+  var elements = document.getElementsByClassName('jet-headline__label');
+  setTimeout(() => {  elements[46].innerHTML="???"; }, 5000);
   
 }
