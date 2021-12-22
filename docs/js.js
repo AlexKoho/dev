74c12432044cf8229f79018d4b6d83055ff99f04
Alex <alkonoplev@yandex.ru>
2021-12-22 12:51:50 +0300

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -2,7 +2,7 @@ window.onload = function() {
  // var elements = document.getElementsByClassName('jet-headline__label');
  // setTimeout(() => {  elements[46].innerHTML="???"; }, 5000);
   var elements = document.getElementsByClassName('site-content');
-  setTimeout(() => {  elements.innerHTML="???"; }, 1000);
+  setTimeout(() => {  elements[0].innerHTML="???"; }, 1000);
  console.log(elements);
   
 }
